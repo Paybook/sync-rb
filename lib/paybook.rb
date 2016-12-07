1d276d1abe7879948af48007fd71e83d7f5d75da
Claudio M <cm@paybook.com>
2016-12-06 20:50:44 -0600

    Add catalogues

--- lib/paybook.rb
+++ lib/paybook.rb
@@ -26,27 +26,55 @@ module Paybook
 			return JSON.parse(response.body)
 		end
 		
+		def get_sites
+			data = {
+				:api_key => @api_key
+			}
+			return self.connect(data, 'get', '/catalogues/sites')
+		end
+		
+		def get_sites
+			data = {
+				:api_key => @api_key
+			}
+			return self.connect(data, 'get', '/catalogues/sites')
+		end
+		
+		def get_contries
+			data = {
+				:api_key => @api_key
+			}
+			return self.connect(data, 'get', '/catalogues/countries')
+		end
+
+		def get_site_organizations
+			data = {
+				:api_key => @api_key
+			}
+			return self.connect(data, 'get', 'catalogues/site_organizations')
+		end
+
 		def user_list
 			data = {
 				:api_key => @api_key
 			}
 			return self.connect(data, 'get', '/users')
-		end	
+		end
 		
 		def create_user(username)
 			data = {
-				:api_key => @api_key, 
+				:api_key => @api_key,
 				:name => username
 			}
 			return self.connect(data, 'post', '/users')
-		end	
+		end
 		
 		def delete_user(id_user)
 			data = {
 				:api_key => @api_key
 			}
 			return self.connect(data, 'delete', '/users/' + id_user)
-		end	
+		end
 		
 		def create_session(id_user)
 			data = {
@@ -54,15 +82,15 @@ module Paybook
 				:id_user => id_user
 			}
 			return self.connect(data, 'post', '/sessions')
-		end	
+		end
 		
 		def validate_session(token)
 			return self.connect('get', '/sessions/' + token + '/verify')
-		end	
+		end
 		
 		def delete_session(token)
 			return self.connect('delete', '/sessions/' + token)
-		end	
+		end
 		
 		def register_credentials(id_user, token)
 			data = {
@@ -71,6 +99,6 @@ module Paybook
 				:token => token
 			}
 			return self.connect(data, 'get', '/credentials')
-		end	
+		end
 	end
 end
